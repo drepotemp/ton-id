bbfdc41d0e9783afb55fdd8b39ba0258beff04f7
fomativeh <ufomaitive@gmail.com>
2024-04-02 10:27:21 +0100

    Fixed server port

--- app.js
+++ app.js
@@ -100,7 +100,7 @@ app.post("/referUser/:referralLink", async (req, res) => {
   }
 });
 
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 7000
 app.listen(port, () => {
   console.log(`App is listening on port ${port}`);
 });
